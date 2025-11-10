11b84aaa3ebe1c3a5576f788a1e599942f3307b7
Rupesh <rupeshchede60@gmail.com>
2025-11-10 23:34:42 +0530

    day-10

--- project/bastion-ec2.tf
+++ project/bastion-ec2.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "bastion_ec2" {
     vpc_security_group_ids = [ aws_security_group.public_sg.id ]
     availability_zone = "us-east-1a"
     key_name = "test"
+    associate_public_ip_address = true
+    
 
     tags = {
       Name = "Bastion server"
--- project/rds.tf
+++ project/rds.tf
@@ -12,21 +12,23 @@ resource "aws_db_subnet_group" "rds_sub_group" {
 # RDS MySQL Database
 # ----------------------------- #
 resource "aws_db_instance" "mysql" {
-    identifier = "my-sql-db"
+    allocated_storage = 10
+    db_name = "Dev"
+    identifier = "dev-db"
     engine = "mysql"
     engine_version = "8.0"
-    instance_class = "db.t3.mirco"
-    allocated_storage = 10
-
+    instance_class = "db.t3.micro"
     username = "admin"
     password = "SkyOps123"
-
-    db_subnet_group_name = aws_db_subnet_group.rds_sub_group.id
-    skip_final_snapshot = true
+    parameter_group_name = "default.mysql8.0"
+    db_subnet_group_name = aws_db_subnet_group.rds_sub_group.name
+    vpc_security_group_ids = [ aws_security_group.rds_sg.id ]
 
     backup_retention_period = 7
     backup_window = "02:00-03:00"
 
     maintenance_window = "sun:04:00-sun:05:00"
+
+    skip_final_snapshot = true
   
 }
